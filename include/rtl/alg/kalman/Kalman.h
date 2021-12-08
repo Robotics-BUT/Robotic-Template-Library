606836ac1875156b7db70a4603022212cf3ee2de
Adam Ligocki <xligoc01@stud.feec.vutbr.cz>
2021-12-08 15:22:00 +0100

    cleaning up after implementing algs

--- include/rtl/alg/kalman/Kalman.h
+++ include/rtl/alg/kalman/Kalman.h
@@ -1,3 +1,29 @@
+// This file is part of the Robotic Template Library (RTL), a C++
+// template library for usage in robotic research and applications
+// under the MIT licence:
+//
+// Copyright 2021 Brno University of Technology
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom
+// the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+// OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+// OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+// Contact person: Adam Ligocki <adam.ligocki@vutbr.cz>
+
 #ifndef ROBOTICTEMPLATELIBRARY_KALMAN_H
 #define ROBOTICTEMPLATELIBRARY_KALMAN_H
 
@@ -50,7 +76,7 @@ namespace rtl
 
         const Matrix<state_dim, 1, dtype>& states() const {return x_states_;}
         const Matrix<state_dim, state_dim, dtype>& covariance() const {return P_covariance_;}
-        const Matrix<state_dim, measurement_dim, dtype>& klman_gain() {return K_kalman_gain_;}
+        const Matrix<state_dim, measurement_dim, dtype>& kalman_gain() {return K_kalman_gain_;}
 
         void set_states(const Matrix<state_dim, 1, dtype>& states) {x_states_ = states;}
         void set_transision_matrix(const Matrix<state_dim, state_dim, dtype>& transition_matrix) {A_transition_matrix_ = transition_matrix;}
